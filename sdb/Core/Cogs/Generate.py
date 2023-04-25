e3ba7f5417acd07257de8621489958953b52c50f
ShyAssassin <49711232+ShyAssassin@users.noreply.github.com>
2023-04-25 15:02:59 +0200

    Update Generate.py

--- sdb/Core/Cogs/Generate.py
+++ sdb/Core/Cogs/Generate.py
@@ -2,20 +2,22 @@ import disnake
 from disnake.ext import commands
 from diffusers import StableDiffusionPipeline
 import torch
-import io
+import os
 from Core import SDB
 
 
 class Generate(commands.Cog):
     def __init__(self, bot: SDB):
         self.bot: SDB = bot
+        # TODO: make these configurable with a config file or slash command
         self.width = 512
         self.height = 512
-        self.sample_steps = 50
+        self.sample_steps = 65
         self.guidance_scale = 7
         self.images_per_promt = 1
         self.negtive_prompts = "(worst quality:1.4), (low quality:1.4), (monochrome:1.1)"
         self.model_id = "andite/anything-v4.0"
+        # self.model_id = "runwayml/stable-diffusion-v1-5"
         self.pipeline = StableDiffusionPipeline.from_pretrained(self.model_id, torch_dtype=torch.float16)
         self.pipeline.safety_checker = lambda images, clip_input: (images, False)
         self.pipeline.to("cuda")
@@ -25,7 +27,7 @@ class Generate(commands.Cog):
     # if we dont defer 3 seconds after the command is sent discord will error out
     # could be fixed by using a queue system that is executed in the background on a worker thread
     @commands.slash_command(name="generate", description="generate a image using provided promts")
-    async def Template(self, interaction: disnake.CommandInteraction, prompt: str):
+    async def Generate(self, interaction: disnake.CommandInteraction, prompt: str):
         await interaction.response.defer(ephemeral=False)
         image = self.pipeline(
             prompt,
@@ -37,12 +39,17 @@ class Generate(commands.Cog):
             num_images_per_prompt=self.images_per_promt,
         ).images
         # FIXME: we shouldnt need to save the image to disk, we should be able to just send it as a byte array
-        image[0].save(f"cache/{interaction.id}.png", format="PNG")
-        await interaction.followup.send(files=[disnake.File(f"cache/{interaction.id}.png", filename=f"{interaction.id}.png")])
+        try:
+            os.mkdir(f"cache/{interaction.guild_id}")
+        except FileExistsError:
+            pass
+        finally:
+            image[0].save(f"cache/{interaction.guild_id}/{interaction.id}.png", format="PNG")
+            await interaction.followup.send(files=[disnake.File(f"cache/{interaction.guild_id}/{interaction.id}.png", filename=f"{interaction.id}.png")])
 
     @commands.command(name="generate")
-    async def TemplateCTX(self, ctx: commands.Context):
-        await ctx.reply(content=f"This is a Template")  # reply, send
+    async def GenerateCTX(self, ctx: commands.Context):
+        await ctx.reply(content=f"Use the slash command!")
 
 
 def setup(bot: SDB):
