48436863822eade6ef036cda8f530eeb6bf11d59
ShyAssassin <49711232+ShyAssassin@users.noreply.github.com>
2023-05-03 23:16:39 +0200

    Added reload command

---
+++ sdb/Core/Cogs/reload.py
@@ -0,0 +1,32 @@
+import disnake
+import os
+from disnake.ext import commands
+from Core import SDB
+
+
+class Reload(commands.Cog):
+    def __init__(self, bot: SDB):
+        self.bot: SDB = bot
+
+    @commands.slash_command(name="reload", description="reload all pipelines with the current config")
+    @commands.check_any(commands.is_owner(), commands.has_permissions(administrator=True))
+    async def Reload(self, interaction: disnake.CommandInteraction):
+        await interaction.response.defer(ephemeral=False)
+        await interaction.edit_original_message(content="Reloading pipelines, this may take a while...")
+        # update config with the latest changes
+        self.bot.config.load()
+        for cog in os.listdir(self.bot.CogPath):
+            if not cog.startswith("_") and cog.endswith(".py") and cog != "reload.py":
+                CurrentCogPath = str(os.path.join(self.bot.CogPath, cog))
+                CurrentCogModule = CurrentCogPath.replace("\\", ".")[:-3]
+                self.bot.reload_extension(CurrentCogModule)
+                print(f"Reloading Extension: sdb.{CurrentCogModule}")
+        await interaction.edit_original_message(content="All pipelines have been reloaded!")
+
+    @commands.command(name="reload")
+    async def ReloadCTX(self, ctx: commands.Context):
+        await ctx.reply(content="This is a Template")  # reply, send
+
+
+def setup(bot: SDB):
+    bot.add_cog(Reload(bot))
