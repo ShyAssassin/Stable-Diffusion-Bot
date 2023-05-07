4542b46a08d42bc1fa3782e82e910e69170b178c
ShyAssassin <49711232+ShyAssassin@users.noreply.github.com>
2023-05-07 14:14:20 +0200

    Update purge.py

--- sdb/Core/Cogs/purge.py
+++ sdb/Core/Cogs/purge.py
@@ -19,26 +19,12 @@ class Purge(commands.Cog):
         await interaction.channel.purge(limit=amount)
         await interaction.edit_original_message(content=f"Purged {amount} messages from {interaction.channel.mention}")
 
-    @commands.slash_command(name="purge-all", description="Purge all content from the current channel")
-    @commands.check_any(commands.is_owner(), commands.has_permissions(administrator=True, manage_messages=True))
-    async def PurgeAll(self, interaction: disnake.CommandInteraction):
-        await interaction.response.defer(ephemeral=True)
-        deleted = await interaction.channel.purge(limit=None)
-        await interaction.edit_original_message(content=f"Purged {len(deleted)} messages from {interaction.channel.mention}")
-
     @commands.slash_command(name="purge-bot", description="Purge a specified amount of bot messages from the current channel")
     @commands.check_any(commands.is_owner(), commands.has_permissions(administrator=True, manage_messages=True))
     async def PurgeBot(self, interaction: disnake.CommandInteraction, amount: int):
         await interaction.response.defer(ephemeral=True)
         await interaction.channel.purge(limit=amount, check=self.is_bot)
         await interaction.edit_original_message(content=f"Purged {amount} messages from {interaction.channel.mention}")
-
-    @commands.slash_command(name="purge-bot-all", description="Purge all bot messages from the current channel")
-    @commands.check_any(commands.is_owner(), commands.has_permissions(administrator=True, manage_messages=True))
-    async def PurgeBotAll(self, interaction: disnake.CommandInteraction):
-        await interaction.response.defer(ephemeral=True)
-        deleted = await interaction.channel.purge(limit=None, check=self.is_bot)
-        await interaction.edit_original_message(content=f"Purged {len(deleted)} messages from {interaction.channel.mention}")
 
     @commands.command(name="purge")
     async def PurgeCTX(self, ctx: commands.Context):
