9fefd057bbab757374491b101d6eb6fbf8a9b337
ShyAssassin <49711232+ShyAssassin@users.noreply.github.com>
2023-06-13 09:19:38 +0200

    Added img2img support

--- sdb/Core/Cogs/img2img.py
+++ sdb/Core/Cogs/img2img.py
@@ -1,53 +1,51 @@
-from diffusers import StableDiffusionPipeline, DPMSolverMultistepScheduler
-from Core import make_collage, SDB, Text2ImgConfig
+from diffusers import StableDiffusionImg2ImgPipeline, DPMSolverMultistepScheduler
+from Core import SDB, Img2ImgConfig, make_collage
 from disnake.ext import commands
-import asyncio
+from io import BytesIO
+from PIL import Image
+import requests
 import disnake
+import asyncio
 import random
 import torch
 import math
-import io
 import os
 
+# -------------------------------------------------------------
+# FIXME: This is nearly identical to text2img.py, refactor this
+# Maybe make a base class for img2img and text2img?
+# -------------------------------------------------------------
 
-class Text2Img(commands.Cog):
+
+class Img2Img(commands.Cog):
     def __init__(self, bot: SDB):
         self.bot: SDB = bot
-        self.config: Text2ImgConfig = self.bot.config.Text2Img
-        self.pipelines: list[StableDiffusionPipeline] = []
+        self.config: Img2ImgConfig = bot.config.Img2Img
+        self.pipelines: list[StableDiffusionImg2ImgPipeline] = []
         self.configure_pipelines()
         self.queue_size = 0
 
     def configure_pipelines(self):
         if self.config.enabled:
             for _ in range(self.config.batch_size):
-                pipeline: StableDiffusionPipeline = StableDiffusionPipeline.from_pretrained(
+                pipeline: StableDiffusionImg2ImgPipeline = StableDiffusionImg2ImgPipeline.from_pretrained(
                     pretrained_model_name_or_path=self.config.model.id,
                     torch_dtype=torch.float16,
-                    custom_pipeline=self.config.custom_pipeline,
-                    custom_revision=self.config.custom_pipeline_revision,
                 )  # type: ignore
-                pipeline.safety_checker = self.safety_checker
-                # we set the scheduler to DPM++ because it is much faster and better than the default scheduler PNDM
+                pipeline.safety_checker = None
                 pipeline.scheduler = DPMSolverMultistepScheduler.from_config(pipeline.scheduler.config, use_karras_sigmas=False)
                 pipeline.to(self.config.device)
                 self.pipelines.append(pipeline)
 
-    def safety_checker(self, images, clip_input):
-        # TODO: implement this
-        if self.config.safety_checker:
-            print("!!! Safety Checker is currently not implemented !!!")
-        return images, False
-
-    def generate_images(self, prompt, negative_prompt, images, seed):
+    def generate_images(self, image, strength, prompt, negative_prompt, images, seed):
         try:
             pipeline = self.pipelines.pop()
             generator = torch.Generator(device=self.config.device).manual_seed(seed)
             images += pipeline(
+                image=image,
                 prompt=prompt,
+                strength=strength,
                 generator=generator,
-                width=self.config.width,
-                height=self.config.height,
                 negative_prompt=negative_prompt,
                 guidance_scale=self.config.guidance_scale,
                 num_inference_steps=self.config.sample_steps,
@@ -55,16 +53,18 @@ class Text2Img(commands.Cog):
             ).images  # type: ignore
             self.pipelines.append(pipeline)
             self.queue_size -= 1
-        # On very very very rare occasions two threads will pop the same pipeline, and not throw a IndexError
+        # On very very very rare occasions two threads will pop the same pipeline, and not throw an IndexError
         # This should never happen because list operations are atomic (doesnt release the GIL), but it does happen, WTF?
         except (RuntimeError, IndexError):
             return
 
-    @commands.slash_command(name="text2img", description="Generate a image using provided prompts")
-    async def text2img(
+    @commands.slash_command(name="img2img", description="Generate a image from a image")
+    async def img2img(
         self,
         interaction: disnake.CommandInteraction,
-        prompt: str,
+        image: disnake.Attachment,
+        strength: float = -1,
+        prompt: str = "",
         negative_prompt: str = "",
         seed: int = -1,
     ):
@@ -75,7 +75,15 @@ class Text2Img(commands.Cog):
 
         await interaction.response.defer(ephemeral=False)
 
-        # if we have a base prompt we should add it to the provided prompt
+        # stength must be between 0 and 1
+        if strength == -1:
+            strength = self.config.strength
+        elif strength < 0 or strength > 1:
+            await interaction.edit_original_message(
+                content="Error: strength must be between 0 and 1, falling back to default value\nGenerating..."
+            )
+            strength = self.config.strength
+
         if self.config.base_prompts != []:
             prompt = f"{str(self.config.base_prompts)}, {prompt}"
 
@@ -85,7 +93,6 @@ class Text2Img(commands.Cog):
         elif self.config.negative_prompts:  # User didn't provide negative prompts, use base negative prompts
             negative_prompt = ", ".join(self.config.negative_prompts)
 
-        # search for banned prompts
         if self.bot.config.banned_prompts != "":
             for banned_prompt in self.bot.config.banned_prompts:
                 if banned_prompt in prompt.lower():
@@ -93,14 +100,17 @@ class Text2Img(commands.Cog):
                     return
 
         self.queue_size += 1
+        response = requests.get(image.url)
+        init_image = Image.open(BytesIO(response.content)).convert("RGB")
+        if self.config.resize_images:
+            init_image = init_image.resize((self.config.width, self.config.height))
         while True:
             if self.pipelines != []:
                 if seed < 0:
-                    seed = random.randint(0, 2**32)  # should we use 64 bit seeds?
+                    seed = random.randint(0, 2**32)
 
-                # generate images in a asyncio thread so we don't block the event loop
                 images = []
-                await asyncio.to_thread(self.generate_images, prompt, negative_prompt, images, seed)
+                await asyncio.to_thread(self.generate_images, init_image, strength, prompt, negative_prompt, images, seed)
 
                 # no images were generated, try again
                 if len(images) > 0:
@@ -118,16 +128,16 @@ class Text2Img(commands.Cog):
             else:
                 rows = math.ceil(math.sqrt(len(images)))
                 cols = math.ceil(len(images) / rows)
-            image = make_collage(images, rows, cols)
+            output_image = make_collage(images, rows, cols)
         else:
-            image = images[0]
+            output_image = images[0]
 
         if self.bot.config.save_images:
             os.makedirs(f"cache/{interaction.guild_id}", exist_ok=True)
-            image.save(f"cache/{interaction.guild_id}/{interaction.id}.png")
+            output_image.save(f"cache/{interaction.guild_id}/{interaction.id}.png")
 
-        with io.BytesIO() as image_binary:
-            image.save(image_binary, "PNG")
+        with BytesIO() as image_binary:
+            output_image.save(image_binary, "PNG")
             image_binary.seek(0)
             image_file = disnake.File(fp=image_binary, filename=f"{interaction.id}.png")
             if self.bot.config.debug:
@@ -139,7 +149,7 @@ class Text2Img(commands.Cog):
                         "fields": [
                             {"name": "Model", "value": f"{self.config.model.name}", "inline": False},
                             {"name": "Seed", "value": f"{seed}", "inline": True},
-                            {"name": "Sample Steps", "value": f"{self.config.sample_steps}", "inline": True},
+                            {"name": "Strength", "value": f"{strength}", "inline": True},
                             {"name": "Guidance Scale", "value": f"{self.config.guidance_scale}", "inline": True},
                             {"name": "Batch Size", "value": f"{self.config.batch_size}", "inline": True},
                             {"name": "IPP", "value": f"{self.config.images_per_prompt}", "inline": True},
@@ -156,4 +166,4 @@ class Text2Img(commands.Cog):
 
 
 def setup(bot: SDB):
-    bot.add_cog(Text2Img(bot))
+    bot.add_cog(Img2Img(bot))
--- sdb/Core/config.py
+++ sdb/Core/config.py
@@ -17,19 +17,17 @@ class Model(BaseModel):
         return v
 
 
-class Text2ImgConfig(BaseModel):
+class BasePipelineConfig(BaseModel):
+    enabled: bool = True
     device: str = "cuda"
     model: Model = Model(name="anything-v4.0", id="andite/anything-v4.0", local=False)
-    custom_pipeline: str = "lpw_stable_diffusion"
-    custom_pipeline_revision: str = "0.15.1"
     width: int = 512
     height: int = 512
-    batch_size: int = 3
+    batch_size: int = 2
     sample_steps: int = 35
-    guidance_scale: int = 7
+    guidance_scale: float = 7
     images_per_prompt: int = 1
     safety_checker: bool = True
-    stack_horizontally: bool = True
     # the pipeline does not like working with arrays, so we should just join it and add ", " between each item before passing it to the pipeline
     # we use a list here because it is easier to work with when in json format
     base_prompts: list[str] = []
@@ -40,11 +38,9 @@ class Text2ImgConfig(BaseModel):
         "mutation",
         "mutated",
     ]
-    # we dont need to join this because we are just checking if the prompt is in the list
-    banned_prompts: list[str] = []
 
     @validator("images_per_prompt")
-    def prompts_per_prompt_valid(cls, v):
+    def images_per_prompt_valid(cls, v):
         if v < 1:
             raise ValueError("Images per prompt must be greater than 0")
         return v
@@ -67,30 +63,48 @@ class Text2ImgConfig(BaseModel):
             raise ValueError("Sample steps must be greater than 0")
         return v
 
+    @validator("width")
+    def width_valid(cls, v):
+        if v < 1:
+            raise ValueError("Width must be greater than 0")
+        return v
+
+    @validator("height")
+    def height_valid(cls, v):
+        if v < 1:
+            raise ValueError("Height must be greater than 0")
+        return v
+
+
+class Text2ImgConfig(BasePipelineConfig):
+    device: str = "cuda"
+    model: Model = Model(name="anything-v4.0", id="andite/anything-v4.0", local=False)
+    custom_pipeline: str = "lpw_stable_diffusion"
+    custom_pipeline_revision: str = "0.15.1"
+    stack_horizontally: bool = True
+
+
+class Img2ImgConfig(BasePipelineConfig):
+    strength: float = 0.5
+    resize_images: bool = True
+    stack_horizontally: bool = True
+
+    @validator("strength")
+    def strength_valid(cls, v):
+        if v < 0 or v > 1:
+            raise ValueError("Strength must be between 0 and 1")
+        return v
+
 
 class SDBConfig(BaseModel):
     version: int = 2
-    debug: bool = False
+    debug: bool = True
     save_images: bool = False
     command_prefix: str = "*"
-    models: list[Model] = [
-        Model(
-            name="anything-v4.0",
-            id="andite/anything-v4.0",
-            local=False,
-        ),
-        Model(
-            name="stable-diffusion-v1-4",
-            id="CompVis/stable-diffusion-v1-4",
-            local=False,
-        ),
-        Model(
-            name="stable-diffusion-v1-5",
-            id="runwayml/stable-diffusion-v1-5",
-            local=False,
-        ),
-    ]
+    banned_prompts: list[str] = []
+
     Text2Img: Text2ImgConfig = Text2ImgConfig()
+    Img2Img: Img2ImgConfig = Img2ImgConfig()
 
     def save(self, file: str = "config.json"):
         with open(file, "w") as f:
@@ -122,3 +136,22 @@ class SDBConfig(BaseModel):
             self.save()
         except (ValidationError, Exception):
             print("Failed to update config with new values!")
+
+
+# models: list[Model] = [
+#     Model(
+#         name="anything-v4.0",
+#         id="andite/anything-v4.0",
+#         local=False,
+#     ),
+#     Model(
+#         name="stable-diffusion-v1-4",
+#         id="CompVis/stable-diffusion-v1-4",
+#         local=False,
+#     ),
+#     Model(
+#         name="stable-diffusion-v1-5",
+#         id="runwayml/stable-diffusion-v1-5",
+#         local=False,
+#     ),
+# ]
